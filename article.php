878504fb02611560edadbb95de99ebb821bc5e6d
Roman <roman.br88@yandex.ru>
2020-06-18 22:34:47 +0300

    l_1_corrections

--- article.php
+++ article.php
@@ -2,11 +2,9 @@
 
 use Models\Article;
 
-spl_autoload_register(function ($class) {
-    require __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
-});
+require __DIR__ . '/autoload.php';
 
-$newsId = isset($_GET['id']) ? $_GET['id'] : 0;
+$newsId = !isset($_GET['id']) ?: $_GET['id'];
 
 $data = Article::findById($newsId);
 
--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 
 use Models\Article;
 
-spl_autoload_register(function ($class) {
-    require __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
-});
+require __DIR__ . '/autoload.php';
 
 $data = Article::getLastNews();
 
